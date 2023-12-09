e3cdd4c0c408bcc5191a5558d41ea8832e7c1303
pkmntrainerfrost <smaarif.achsien@gmail.com>
2023-12-09 09:22:03 +0700

    feat: rekap

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import json
+from pydantic import BaseModel
+import datetime
+
+class Transaction(BaseModel):
+    id: int
+    type: str
+    transaction_from: str
+    description: str
+    amount: int
+    date: datetime.date
+
+json_filename="transactions.json"
+
+with open(json_filename,"r") as read_file:
+	data = json.load(read_file)
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"Hello": "World"}
+
+@app.get('/recap')
+async def financial_recap():
+	
+    total_incoming = 0
+    total_outgoing = 0
+
+    for i in data["transactions"]:
+        if i["type"] == "incoming":
+             total_incoming += i["amount"]
+        else:
+             total_outgoing += i["amount"]
+
+    return {"total_incoming" : total_incoming, "total_outgoing" : total_outgoing, "transactions" : data["transactions"]}
